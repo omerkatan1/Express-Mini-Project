3213b5b3dd8a7aa437fcca2a4d3c235b266f5738
Omer Katan <omerkatan@live.com>
2019-11-23 14:03:01 -0800

    adding server stuff

--- Assets/server.js
+++ Assets/server.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const path = require('path');
-const app = express(); 
+const app = express();
 
 app.use(express.static('../public'));
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// setup routes for dynamic data
+app.get("/api/reservations", function (req, res) {
+    // TODO: retrieve reservations and send them back as JSON
+    console.log(JSON.stringify(res.json));
+});
+
+// setup routes for dynamic data
+app.post("/api/reservations", function (req, res) {
+    // TODO: receive data and create reservation
+    res.json(req.body);
+});
 
 app.listen(8080, () => {
     console.log(`Server folder: ${__dirname}`);
     console.log(`Static root folder should be: ${__dirname + '/../public'}`);
 });
+
