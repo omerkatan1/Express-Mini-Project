37be98f3607636e296e645a910c0bd40b10301b9
Omer Katan <omerkatan@live.com>
2019-11-23 12:44:58 -0800

    server working

--- Assets/server.js
+++ Assets/server.js
@@ -1,3 +1,11 @@
 const express = require('express');
-const app = express();
+const path = require('path');
+const app = express(); 
 
+app.use(express.static('../public'));
+
+
+app.listen(8080, () => {
+    console.log(`Server folder: ${__dirname}`);
+    console.log(`Static root folder should be: ${__dirname + '/../public'}`);
+});
